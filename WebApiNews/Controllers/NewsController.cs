79f79a2577f193dc90a6e7b5957b6280baad0db7
Ivan Lara <uki@live.co.uk>
2018-12-08 14:36:35 -0600

    Finalizando metodos API

--- WebApiNews/Controllers/NewsController.cs
+++ WebApiNews/Controllers/NewsController.cs
@@ -11,15 +11,27 @@ namespace WebApiNews.Controllers
     public class NewsController : ApiController
     {
         // GET: api/News
-        public List<News> Get()
+        [Route("api/News")]
+        public List<News> GetRecents()
         {
             using (var database = new Context())
             {
-                return database.TheNews.OrderBy(d => d.Post).ToList();
+                return database.TheNews.OrderByDescending(d => d.Post).Take(4).ToList();
+            }
+        }
+
+        // GET: api/NewsAll
+        [Route("api/NewsAll")]
+        public List<News> GetAll()
+        {
+            using (var database = new Context())
+            {
+                return database.TheNews.OrderByDescending(d => d.Post).ToList();
             }
         }
 
         // GET: api/News/5
+        [Route("api/News/{id}")]
         public News Get(int id)
         {
             using (var database = new Context())
